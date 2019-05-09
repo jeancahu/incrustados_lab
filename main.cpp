63a37e09361384ffb2aa3f257bc16fed5493f8fe
Carolina Trejos <carotq777@gmail.com>
2019-05-08 18:46:09 -0600

    Add light sensor and microphone logic

--- main.cpp
+++ main.cpp
@@ -4,26 +4,51 @@
 //
 //****************************************************************************
 
-#include "msp.h"
+
 #include "main.hpp"
+#include "msp.h"
+
 #include "HAL_OPT3001.h"
+
+#include "i2c.h"
+#include "adc14.h"
+#include "gpio.h"
 // ****************
 // Global variables
 // ****************
 uint32_t g_counter;
 uint32_t g_reboundCtl_counter;
-bool g_ctrButton;
+uint32_t g_counterMicSampler;
+uint8_t  g_noiseBuffer;
+//uint8_t  g_noiseBufferArray[NOISE_ARRAY_LEN];
+//uint8_t  g_noiseBufferIndex;
 
+bool g_ctrButton;
+bool g_light_flag;
+bool g_noise_flag;
 // Lamp status
-bool g_lamp_status;
+//bool g_lamp_status;
 
 void main(void)
 {
   // - Run the overall setup function for the system
   setup();
   systemConfirm();
+  startADC();
   while (1) {
-    // LAMP = 0x0000;
+    if ( g_noiseBuffer == 0xFF )
+      {
+    	g_noise_flag = true;
+      } else {
+      g_noise_flag = false;
+
+    }
+    if ( OPT3001_getLux() > LUX_LIMIT )
+      {
+	g_light_flag = true;
+      } else {
+      g_light_flag = false;
+    }
   };
 }
 
@@ -46,7 +71,7 @@ static void setup(void)
   g_counter = ZERO;
   g_reboundCtl_counter = ZERO;
   g_ctrButton = false;
-  
+
   // ****************************
   //         PORT CONFIG
   // ****************************
@@ -85,10 +110,26 @@ static void setup(void)
   // ****************************
   //       ADC CONFIG
   // ****************************
-  // - Configure ADC1
-  // - Select the ADC input channel and the ADC clock division
-  //P5->SEL1 |= BIT4;                           // Configure P5.4 for ADC
-  //P5->SEL0 |= BIT4;
+  initADC();
+  initMicrophone();
+
+  g_noiseBuffer = ZERO;
+	//for ( int i = 0; i<NOISE_ARRAY_LEN ; i++ )
+	//{
+	//	g_noiseBufferArray[i] = 0xFF;
+	//}
+
+  // ****************************
+  //         I2C CONFIG
+  // - Initialize I2C communication */
+  // ****************************
+  Init_I2C_GPIO();
+  I2C_init();
+  // ****************************
+  //         DEVICE CONFIG
+  // - Initialize OPT3001 digital ambient light sensor */
+  // ****************************
+  OPT3001_init();
 
 
   // Enables all interrupts
@@ -108,19 +149,294 @@ static void systemConfirm ()
   return;
 }
 
+
+static void initADC() {
+    ADC14_enableModule();
+
+    // This sets the conversion clock to 3MHz
+    ADC14_initModule(ADC_CLOCKSOURCE_ADCOSC,
+                     ADC_PREDIVIDER_1,
+                     ADC_DIVIDER_1,
+                      0
+                     );
+
+    ADC14_setResolution(ADC_14BIT);
+
+    // This configures the ADC to store output results
+    // in ADC_MEM0 (single-channel conversion, repeat mode)
+    ADC14_configureSingleSampleMode(ADC_MEM0, true);
+
+    // This configures the ADC in automatic conversion mode
+    ADC14_enableSampleTimer(ADC_AUTOMATIC_ITERATION);
+}
+
+void startADC() {
+    // The ADC is in continuous sampling mode, so after calling this once
+    // the ADC will continuously update
+    ADC14_enableConversion();
+    ADC14_toggleConversionTrigger();
+}
+
+void initMicrophone() {
+    // This configures ADC_MEM0 to store the result from
+    // input channel A10 (Microphone), in non-differential input mode
+    // (non-differential means: only a single input pin)
+    // The reference for Vref- and Vref+ are VSS and VCC respectively
+    ADC14_configureConversionMemory(ADC_MEM0,
+                                    ADC_VREFPOS_AVCC_VREFNEG_VSS,
+                                    ADC_INPUT_A10,
+                                    ADC_NONDIFFERENTIAL_INPUTS);
+
+    // This selects the GPIO as analog input
+    // A9 is multiplexed on GPIO port P4 pin PIN4
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P4,
+                                               GPIO_PIN3,
+                                               GPIO_TERTIARY_MODULE_FUNCTION);
+}
+
+unsigned getSampleMicrophone() {
+    return ADC14_getResult(ADC_MEM0);
+}
+
+
+
+/* I2C Master Configuration Parameter */
+const eUSCI_I2C_MasterConfig i2cConfig =
+{
+        EUSCI_B_I2C_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
+        48000000,                               // SMCLK = 48MHz
+        EUSCI_B_I2C_SET_DATA_RATE_400KBPS,      // Desired I2C Clock of 100khz
+        0,                                      // No byte counter threshold
+        EUSCI_B_I2C_NO_AUTO_STOP                // No Autostop
+};
+
+void Init_I2C_GPIO()
+{
+    /* Select I2C function for I2C_SCL(P6.5) & I2C_SDA(P6.4) */
+    GPIO_setAsPeripheralModuleFunctionOutputPin(
+            GPIO_PORT_P6,
+            GPIO_PIN5,
+            GPIO_PRIMARY_MODULE_FUNCTION);
+
+    GPIO_setAsPeripheralModuleFunctionOutputPin(
+            GPIO_PORT_P6,
+            GPIO_PIN4,
+            GPIO_PRIMARY_MODULE_FUNCTION);
+}
+
+
+/***************************************************************************//**
+ * @brief  Configures I2C
+ * @param  none
+ * @return none
+ ******************************************************************************/
+
+void I2C_init(void)
+{
+        /* Initialize USCI_B0 and I2C Master to communicate with slave devices*/
+    I2C_initMaster(EUSCI_B1_BASE, &i2cConfig);
+
+    /* Disable I2C module to make changes */
+    I2C_disableModule(EUSCI_B1_BASE);
+
+    /* Enable I2C Module to start operations */
+    I2C_enableModule(EUSCI_B1_BASE);
+
+    return;
+}
+
+
+/***************************************************************************//**
+ * @brief  Reads data from the sensor
+ * @param  writeByte Address of register to read from
+ * @return Register contents
+ ******************************************************************************/
+
+int I2C_read16(unsigned char writeByte)
+{
+    volatile int val = 0;
+    volatile int valScratch = 0;
+
+    /* Set master to transmit mode PL */
+    I2C_setMode(EUSCI_B1_BASE,
+        EUSCI_B_I2C_TRANSMIT_MODE);
+
+    /* Clear any existing interrupt flag PL */
+    I2C_clearInterruptFlag(EUSCI_B1_BASE,
+        EUSCI_B_I2C_TRANSMIT_INTERRUPT0);
+
+    /* Wait until ready to write PL */
+    while (I2C_isBusBusy(EUSCI_B1_BASE));
+
+    /* Initiate start and send first character */
+    I2C_masterSendMultiByteStart(EUSCI_B1_BASE, writeByte);
+
+    /* Wait for TX to finish */
+    while(!(I2C_getInterruptStatus(EUSCI_B1_BASE,
+        EUSCI_B_I2C_TRANSMIT_INTERRUPT0)));
+
+    /* Initiate stop only */
+    I2C_masterSendMultiByteStop(EUSCI_B1_BASE);
+
+    /* Wait for Stop to finish */
+    while(!I2C_getInterruptStatus(EUSCI_B1_BASE,
+        EUSCI_B_I2C_STOP_INTERRUPT));
+
+    /*
+     * Generate Start condition and set it to receive mode.
+     * This sends out the slave address and continues to read
+     * until you issue a STOP
+     */
+    I2C_masterReceiveStart(EUSCI_B1_BASE);
+
+    /* Wait for RX buffer to fill */
+    while(!(I2C_getInterruptStatus(EUSCI_B1_BASE,
+        EUSCI_B_I2C_RECEIVE_INTERRUPT0)));
+
+    /* Read from I2C RX register */
+    val = I2C_masterReceiveMultiByteNext(EUSCI_B1_BASE);
+
+    /* Receive second byte then send STOP condition */
+    valScratch = I2C_masterReceiveMultiByteFinish(EUSCI_B1_BASE);
+
+    /* Shift val to top MSB */
+    val = (val << 8);
+
+    /* Read from I2C RX Register and write to LSB of val */
+    val |= valScratch;
+
+    /* Return temperature value */
+    return (int16_t)val;
+}
+
+
+/***************************************************************************//**
+ * @brief  Writes data to the sensor
+ * @param  pointer  Address of register you want to modify
+ * @param  writeByte Data to be written to the specified register
+ * @return none
+ ******************************************************************************/
+
+void I2C_write16 (unsigned char pointer, unsigned int writeByte)
+{
+    /* Set master to transmit mode PL */
+    I2C_setMode(EUSCI_B1_BASE,
+        EUSCI_B_I2C_TRANSMIT_MODE);
+
+    /* Clear any existing interrupt flag PL */
+    I2C_clearInterruptFlag(EUSCI_B1_BASE,
+        EUSCI_B_I2C_TRANSMIT_INTERRUPT0);
+
+    /* Wait until ready to write PL */
+    while (I2C_isBusBusy(EUSCI_B1_BASE));
+
+    /* Initiate start and send first character */
+    I2C_masterSendMultiByteStart(EUSCI_B1_BASE,
+        pointer);
+
+    /* Send the MSB to SENSOR */
+    I2C_masterSendMultiByteNext(EUSCI_B1_BASE,
+        (unsigned char)(writeByte>>8));
+
+    I2C_masterSendMultiByteFinish(EUSCI_B1_BASE,
+        (unsigned char)(writeByte&0xFF));
+
+}
+
+
+void I2C_setslave(unsigned int slaveAdr)
+{
+    /* Specify slave address for I2C */
+    I2C_setSlaveAddress(EUSCI_B1_BASE,
+        slaveAdr);
+
+    /* Enable and clear the interrupt flag */
+    I2C_clearInterruptFlag(EUSCI_B1_BASE,
+        EUSCI_B_I2C_TRANSMIT_INTERRUPT0 + EUSCI_B_I2C_RECEIVE_INTERRUPT0);
+    return;
+}
+
+
+void OPT3001_init()
+{
+    /* Specify slave address for OPT3001 */
+    I2C_setslave(OPT3001_SLAVE_ADDRESS);
+    /* Set Default configuration for OPT3001*/
+    I2C_write16(CONFIG_REG, DEFAULT_CONFIG_100);
+}
+
+unsigned long int OPT3001_getLux()
+{
+    /* Specify slave address for OPT3001 */
+    I2C_setslave(OPT3001_SLAVE_ADDRESS);
+
+    uint16_t exponent = 0;
+    uint32_t result = 0;
+    int16_t raw;
+    raw = I2C_read16(RESULT_REG);
+    /*Convert to LUX*/
+    //extract result & exponent data from raw readings
+    result = raw&0x0FFF;
+    exponent = (raw>>12)&0x000F;
+    //convert raw readings to LUX
+    switch(exponent){
+    case 0: //*0.015625
+        result = result>>6;
+        break;
+    case 1: //*0.03125
+        result = result>>5;
+        break;
+    case 2: //*0.0625
+        result = result>>4;
+        break;
+    case 3: //*0.125
+        result = result>>3;
+        break;
+    case 4: //*0.25
+        result = result>>2;
+        break;
+    case 5: //*0.5
+        result = result>>1;
+        break;
+    case 6:
+        result = result;
+        break;
+    case 7: //*2
+        result = result<<1;
+        break;
+    case 8: //*4
+        result = result<<2;
+        break;
+    case 9: //*8
+        result = result<<3;
+        break;
+    case 10: //*16
+        result = result<<4;
+        break;
+    case 11: //*32
+        result = result<<5;
+        break;
+    }
+    return result;
+}
+
+
 extern "C"
 {
   // - Handle the Timer32 Interrupt
   void T32_INT1_IRQHandler(void)
   {
-    TIMER32_1->INTCLR = 0U;    
+    TIMER32_1->INTCLR = 0U;
+
     if ( g_counter )
       { g_counter--; }
     else { LAMP = ZERO; }
 
     if ( g_reboundCtl_counter )
       { g_reboundCtl_counter--; }
-    else if (( P3->IN & BIT5 ) && ( g_ctrButton ))
+    else if ( (( P3->IN & BIT5 ) && ( g_ctrButton )) |\
+    		( g_noise_flag & !( LAMP & (LAMP_1|LAMP_2|LAMP_3)) &\
+    				!g_light_flag))
       {
 	if ( g_counter )
 	  {
@@ -137,6 +453,36 @@ extern "C"
 	}
 	g_ctrButton = false;
       }
+
+    // Microphone
+    if ( g_counterMicSampler )
+      {
+    	g_counterMicSampler--;
+      }
+    else
+      {
+    	g_counterMicSampler = SOUND_TIMER_INT;
+
+        //g_noiseBufferIndex++;
+        //g_noiseBufferArray[g_noiseBufferIndex&3] = getSampleMicrophone();
+
+    	//uint8_t ave_sound = 0;
+    	//for ( int i = 0; i<NOISE_ARRAY_LEN ; i++ )
+    	//{
+	//ave_sound &= g_noiseBufferArray[i];
+    	//}
+    	//ave_sound = ave_sound >> 5;
+    	if ( getSampleMicrophone() > SOUND_LIMIT )
+	  {
+	    // Silence
+            g_noiseBuffer = g_noiseBuffer<<1;
+            //LAMP=0;
+	  } else {
+	  // Noise
+	  g_noiseBuffer = ( g_noiseBuffer<<1 ) | 0x01;
+	  //LAMP=LAMP_2;
+    	}
+      }
     return;
   }
 
@@ -150,11 +496,13 @@ extern "C"
 	// Suppress bounding
 	g_reboundCtl_counter = REB_CTL;
 	g_ctrButton = true;
+	g_noiseBuffer = ZERO;
+	g_noise_flag = false;
       }
     // Clear interrupt flag for all terminals
     P3->IFG &= ~(BIT5);
     // Enable P3.6 interrupt
-    P3->IE |= (BIT5);  
+    P3->IE |= (BIT5);
     return;
   }
 
--- main.hpp
+++ main.hpp
@@ -14,6 +14,13 @@
 #define LAMP_2 BIT1
 #define LAMP_3 BIT2
 
+// 0xFFFF -> Totally Silence;
+// 0x0000 -> Totally Noise
+//#define SOUND_LIMIT 0x1F99
+#define SOUND_LIMIT 0x1F99 // FIX-ME
+#define NOISE_ARRAY_LEN 4
+
+#define SOUND_TIMER_INT 625 // Take a sample every 0.625 s
 //#define HALF_HOUR 1800000 // half-hour in ms
 #define HALF_HOUR 5000 // FIX-ME
 #define REB_CTL 40 // 40 ms
@@ -23,5 +30,39 @@
 // The main Setup function for the application
 static void setup(void);
 static void systemConfirm(void);
+static void initADC(void);
+static void startADC(void);
+static void initMicrophone(void);
+unsigned getSampleMicrophone(void);
+
+void Init_I2C_GPIO(void);
+void I2C_init(void);
+int I2C_read16(unsigned char);
+void I2C_write16(unsigned char pointer, unsigned int writeByte);
+void I2C_setslave(unsigned int slaveAdr);
+
+/*CONSTANTS*/
+#define OPT3001_SLAVE_ADDRESS 0x44
+
+#define OPT_INTERRUPT_PIN 11
+#define RESULT_REG 0x00
+#define CONFIG_REG 0x01
+#define LOWLIMIT_REG 0x02
+#define HIGHLIMIT_REG 0x03
+#define MANUFACTUREID_REG 0x7E
+#define DEVICEID_REG 0x7F
+
+#define DEFAULT_CONFIG 0xCC10 // 800ms
+#define DEFAULT_CONFIG_100 0xC410 // 100ms
+
+void OPT3001_init(void);
+unsigned long int OPT3001_getLux(void);
+//unsigned int OPT3001_readManufacturerId(void);
+//signed int OPT3001_readDeviceId(void);
+//unsigned int OPT3001_readConfigReg(void);
+//unsigned int OPT3001_readLowLimitReg(void);
+//unsigned int OPT3001_readHighLimitReg(void);
+
+#define LUX_LIMIT 0x00FF
 
 #endif /* MAIN_HPP_ */
